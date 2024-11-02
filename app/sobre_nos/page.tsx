8f235623964b0a8d45dac97aaac1d631c1a28cef
20005288 <20005288@maua.br>
2024-11-02 16:45:18 -0300

    pagina inical completa

---
+++ app/sobre_nos/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+
+import { buttonVariants } from "@/components/ui/button"
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <p>Olá</p>
+  );
+}
